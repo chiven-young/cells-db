a5c590a5b702c3a3fb7e54e9d5c12c688f3f67a9
Chiven Young <chiven@yeah.net>
2024-12-24 15:39:03 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Workspace from './modules/workspace';
+import Cells from './modules/cells';
+
+export default class CellsDB {
+    static workspace = Workspace;
+    static cells = Cells;
+}
---
+++ src/modules/workspace.js
@@ -0,0 +1,9 @@
+import PouchDB from 'pouchdb';
+import PouchDBFind from 'pouchdb-find';
+
+PouchDB.plugin(PouchDBFind);
+import fs from 'fs';
+
+export default class Workspace {
+
+}
